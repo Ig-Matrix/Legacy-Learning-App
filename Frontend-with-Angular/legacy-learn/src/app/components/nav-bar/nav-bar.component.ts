55334fa4763d14c402d78c03aec023cecf34d754
Tonyrealzy <umehobiarinze2@gmail.com>
2024-03-10 15:26:04 +0100

    Styled the navbar

--- Frontend-with-Angular/legacy-learn/src/app/components/nav-bar/nav-bar.component.ts
+++ Frontend-with-Angular/legacy-learn/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faList } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  isNavActive = false;
+  barsIcon = faList;
 
+  constructor(private elRef: ElementRef) {}
+
+  toggleNavBar() {
+    this.isNavActive = !this.isNavActive;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    const targetElement = event.target as HTMLElement;
+
+    if (!this.elRef.nativeElement.contains(targetElement)) {
+      this.isNavActive = false;
+    }
+  }
 }
