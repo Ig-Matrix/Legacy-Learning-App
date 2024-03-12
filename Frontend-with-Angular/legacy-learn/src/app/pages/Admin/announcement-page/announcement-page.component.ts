22633042d5afc27d734af304b22e15c88b015752
Tonyrealzy <umehobiarinze2@gmail.com>
2024-03-12 13:44:03 +0100

    Done with creating Pages

--- Frontend-with-Angular/legacy-learn/src/app/pages/Admin/announcement-page/announcement-page.component.ts
+++ Frontend-with-Angular/legacy-learn/src/app/pages/Admin/announcement-page/announcement-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from "../../../components/side-bar/side-bar.component";
+import { HomeNavigationComponent } from "../../../components/home-navigation/home-navigation.component";
 
 @Component({
-  selector: 'app-announcement-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './announcement-page.component.html',
-  styleUrl: './announcement-page.component.css'
+    selector: 'app-announcement-page',
+    standalone: true,
+    templateUrl: './announcement-page.component.html',
+    styleUrl: './announcement-page.component.css',
+    imports: [SideBarComponent, HomeNavigationComponent]
 })
 export class AnnouncementPageComponent {
 
