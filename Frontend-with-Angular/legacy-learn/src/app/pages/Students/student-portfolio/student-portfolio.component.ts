4791d04b8e63fb700e32881662eb21aba837f8c7
Ig-matrix <omomaighawosa440@gmail.com>
2024-03-14 11:06:14 +0100

    render course grades based on logic

--- Frontend-with-Angular/legacy-learn/src/app/pages/Students/student-portfolio/student-portfolio.component.ts
+++ Frontend-with-Angular/legacy-learn/src/app/pages/Students/student-portfolio/student-portfolio.component.ts
@@ -7,9 +7,8 @@ import { AddCourseModalComponent } from '../add-course-modal/add-course-modal.co
   standalone: true,
   imports: [AddCourseModalComponent],
   templateUrl: './student-portfolio.component.html',
-  styleUrl: './student-portfolio.component.css'
+  styleUrl: './student-portfolio.component.css',
 })
-
 export class StudentPortfolioComponent {
   courses: Course[] = []; // Array to store course data
   showModal: boolean = false; // Flag to control modal visibility
@@ -19,9 +18,26 @@ export class StudentPortfolioComponent {
   }
 
   addCourse(course: Course) {
+    course.grade=this.calculateGrade(course.score)
     this.courses.push(course);
     // console.log(course);
-     // Add new course to the array
+    // Add new course to the array
     this.showModal = false; // Close the modal after adding
+  }
+
+  calculateGrade(score: number): string {
+    if (score >= 70) {
+      return 'A';
+    } else if (score >= 60) {
+      return 'B';
+    } else if (score >= 50) {
+      return 'C';
+    } else if (score >= 45) {
+      return 'D';
+    } else if (score >= 40) {
+      return 'E';
+    } else {
+      return 'F';
+    }
   }
 }
