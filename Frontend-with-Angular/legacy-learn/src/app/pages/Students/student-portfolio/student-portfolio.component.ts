3c2b588bcfa52c1c4e04592b26c6f7456df30cbf
Ig-matrix <omomaighawosa440@gmail.com>
2024-03-14 12:02:13 +0100

    add cgpa calculation logic

--- Frontend-with-Angular/legacy-learn/src/app/pages/Students/student-portfolio/student-portfolio.component.ts
+++ Frontend-with-Angular/legacy-learn/src/app/pages/Students/student-portfolio/student-portfolio.component.ts
@@ -12,16 +12,16 @@ import { AddCourseModalComponent } from '../add-course-modal/add-course-modal.co
 export class StudentPortfolioComponent {
   courses: Course[] = []; // Array to store course data
   showModal: boolean = false; // Flag to control modal visibility
+  cgpa: number | null = null; // CGPA property to store calculated value
 
   openAddCourseModal() {
     this.showModal = true; // Open the modal on button click
   }
 
   addCourse(course: Course) {
-    course.grade=this.calculateGrade(course.score)
-    this.courses.push(course);
-    // console.log(course);
-    // Add new course to the array
+    course.grade = this.calculateGrade(course.score);
+    this.courses.push(course); // Add new course to the array
+    this.cgpa = null;
     this.showModal = false; // Close the modal after adding
   }
 
@@ -39,5 +39,43 @@ export class StudentPortfolioComponent {
     } else {
       return 'F';
     }
+  }
+
+  calculateCGPA(): number | null {
+    if (!this.courses.length) {
+      return null; // No courses for calculation
+    }
+
+    let totalCreditPoints = 0;
+    let totalQualityPoints = 0;
+
+    for (const course of this.courses) {
+      const gradePoints = this.getGradePoints(course.score);
+      if (gradePoints !== null) {
+        totalCreditPoints += course.unit;
+        totalQualityPoints += gradePoints * course.unit;
+      }
+    }
+
+    this.cgpa = totalQualityPoints / totalCreditPoints;
+    return totalQualityPoints / totalCreditPoints || null; // Handle division by zero
+  }
+
+  getGradePoints(score: number): number | null {
+    const gradeMap = {
+      '70-100': 5.0,
+      '60-69': 4.0,
+      '50-59': 3.0,
+      '45-49': 2.0,
+      '40-44': 1.0,
+      '0-39': 0.0,
+    };
+
+    const gradeRange = Object.keys(gradeMap).find((range) => {
+      const [min, max] = range.split('-').map(Number);
+      return score >= min && score <= max;
+    });
+
+    return gradeRange ? gradeMap[gradeRange as keyof typeof gradeMap] : null;
   }
 }
