b7c7372086186453001d250276b8d710952eba37
Tonyrealzy <umehobiarinze2@gmail.com>
2024-03-17 20:01:41 +0100

    Done with partitioning users into Admin and Student

---
+++ Frontend-with-Angular/legacy-learn/src/app/model/announcement.model.ts
@@ -0,0 +1,7 @@
+export class Announcement {
+    constructor(
+       public announcementContent: string,
+       public createdAt: Date
+    ) {}
+  }
+  
--- Frontend-with-Angular/legacy-learn/src/app/services/AnnouncementService/announcement.service.ts
+++ Frontend-with-Angular/legacy-learn/src/app/services/AnnouncementService/announcement.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Announcement } from '../../model/announcement.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AnnouncementService {
+  private baseUrl: string = 'http://localhost:8080/api';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  getAnnouncements(): Observable<Announcement[]> {
+    return this.http.get<Announcement[]>(`${this.baseUrl}/getAnnouncement`);
+  }
+
 }
