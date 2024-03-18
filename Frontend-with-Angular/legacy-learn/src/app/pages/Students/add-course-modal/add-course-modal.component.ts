279ceea3902dcf79f146e57cef4853e246dbaedf
Ig-matrix <omomaighawosa440@gmail.com>
2024-03-18 08:02:05 +0100

    add validation for score and unit

--- Frontend-with-Angular/legacy-learn/src/app/pages/Students/add-course-modal/add-course-modal.component.ts
+++ Frontend-with-Angular/legacy-learn/src/app/pages/Students/add-course-modal/add-course-modal.component.ts
@@ -6,13 +6,22 @@ import {
   Output,
 } from '@angular/core';
 import { Course } from '../../../../models/Interfaces/Course';
-import { FormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-add-course-modal',
   standalone: true,
-  imports: [FormsModule, NgClass],
+  imports: [FormsModule, NgClass, ReactiveFormsModule],
   templateUrl: './add-course-modal.component.html',
   styleUrl: './add-course-modal.component.css',
 })
@@ -21,7 +30,30 @@ export class AddCourseModalComponent {
   @Output() courseAdded = new EventEmitter<Course>(); // Event emitter for course
   @Output() closeModalEvent = new EventEmitter();
 
+  courseForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    code: new FormControl('', [Validators.required]),
+    score: new FormControl('', [Validators.required, this.scoreValidator, this.numberValidator]),
+    unit: new FormControl('', [Validators.required, this.numberValidator]),
+  });
+
   course: Course = { name: '', code: '', score: 0, unit: 0 }; // New course object
+
+  scoreValidator(control: AbstractControl): ValidationErrors | null {
+    const score = control.value;
+    if (score < 0 || score > 100) {
+      return { scoreRange: 'Score must be between 0 and 100' };
+    }
+    return null;
+  }
+
+  numberValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    if (typeof value !== 'number') {
+      return { valueType: 'must be a number' };
+    }
+    return null;
+  }
 
   closeModal(event: Event) {
     this.showModal = false; // Hide the modal on close button click
@@ -42,7 +74,7 @@ export class AddCourseModalComponent {
   }
 
   submitCourse(event: Event) {
-    this.courseAdded.emit(this.course); // Emit the course object on submit
+    this.courseAdded.emit(this.courseForm.value); // Emit the course object on submit
     this.closeModal(event); // Close the modal after submission
   }
 }
