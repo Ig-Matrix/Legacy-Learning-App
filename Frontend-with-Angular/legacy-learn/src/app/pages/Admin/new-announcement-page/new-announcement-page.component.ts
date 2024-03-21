234fc52215a3e7482005b44bfe06893571f8c876
Ig-matrix <omomaighawosa440@gmail.com>
2024-03-21 18:58:13 +0100

    finally implemented auto refresh and reload of data once an announcement is created(or modal closed) truly proud of myself for the steeze used here man

--- Frontend-with-Angular/legacy-learn/src/app/pages/Admin/new-announcement-page/new-announcement-page.component.ts
+++ Frontend-with-Angular/legacy-learn/src/app/pages/Admin/new-announcement-page/new-announcement-page.component.ts
@@ -1,9 +1,9 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { HomeNavigationComponent } from "../../../components/home-navigation/home-navigation.component";
-import { Router, RouterLink, RouterLinkActive} from '@angular/router';
-import { AnnouncementListComponent } from "../../../components/announcement-list/announcement-list.component";
-import { AnnouncementDetailsComponent } from "../../../components/announcement-details/announcement-details.component";
-import { AnnouncementFormComponent } from "../../../components/announcement-form/announcement-form.component";
+import { HomeNavigationComponent } from '../../../components/home-navigation/home-navigation.component';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AnnouncementListComponent } from '../../../components/announcement-list/announcement-list.component';
+import { AnnouncementDetailsComponent } from '../../../components/announcement-details/announcement-details.component';
+import { AnnouncementFormComponent } from '../../../components/announcement-form/announcement-form.component';
 import { Announcement } from '../../../../models/announcement.model';
 import { AnnouncementService } from '../../../services/AnnouncementService/announcement.service';
 import { CommonModule } from '@angular/common';
@@ -15,18 +15,27 @@ import { AnnouncementModalComponent } from '../../../components/announcement-mod
   standalone: true,
   templateUrl: './new-announcement-page.component.html',
   styleUrl: './new-announcement-page.component.css',
-  imports: [RouterLink, RouterLinkActive, HomeNavigationComponent, AnnouncementModalComponent, AnnouncementListComponent],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    HomeNavigationComponent,
+    AnnouncementModalComponent,
+    AnnouncementListComponent,
+  ],
 })
 export class NewAnnouncementPageComponent {
   isShowModal: boolean = false;
   @Input() announcements: Announcement[] = [];
   @Output() selectedAnnouncement = new EventEmitter<Announcement>();
+  onAnnouncement: boolean = false;
 
   openModal() {
     this.isShowModal = true;
+    this.onAnnouncement = false;
   }
 
   closeModal() {
     this.isShowModal = false;
+    this.onAnnouncement = true;
   }
 }
